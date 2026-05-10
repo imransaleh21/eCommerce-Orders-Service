5c3e634501774dfd672d77585118370cfd85c863
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2026-05-11 00:31:40 +0600

    Add mongo db connection to the DI container with dynamic ENV values

--- OrdersMicroService.DataAccessLayer/DependencyInjection.cs
+++ OrdersMicroService.DataAccessLayer/DependencyInjection.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 
 namespace OrdersMicroService.DataAccessLayer;
 public static class DependencyInjection
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
-        //services.AddScoped<IOrdersRepository, OrdersRepository>();
+        var mongoDbConnectionTemp = configuration.GetConnectionString("MongoDbConnection")!;
+        var mongoDbConnection = mongoDbConnectionTemp.Replace("$MONGO_HOST", Environment.GetEnvironmentVariable("MONGO_HOST"))
+                                                     .Replace("$MONGO_PORT", Environment.GetEnvironmentVariable("MONGO_PORT"));
+        services.AddSingleton<IMongoClient>(new MongoClient(mongoDbConnection));
+        services.AddScoped<IMongoDatabase>(provider =>
+        {
+            IMongoClient client = provider.GetRequiredService<IMongoClient>();
+            return client.GetDatabase("OrdersDataBase");
+        });
         return services;
     }
 }
