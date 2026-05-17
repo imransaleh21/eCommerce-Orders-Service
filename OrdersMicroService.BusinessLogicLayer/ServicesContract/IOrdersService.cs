c1fdd21085447f76d41c5983dfd683df05ff5094
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2026-05-18 00:28:01 +0600

    Add services contract for order service

---
+++ OrdersMicroService.BusinessLogicLayer/ServicesContract/IOrdersService.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using OrdersMicroService.BusinessLogicLayer.DTOs;
+using OrdersMicroService.DataAccessLayer.Entities;
+
+namespace OrdersMicroService.BusinessLogicLayer.ServicesContract;
+public interface IOrdersService
+{
+    Task<List<OrderResponse?>> GetOrders();
+    Task<List<OrderResponse?>> GetOrdersByCondition(FilterDefinition<Order> filter);
+    Task<OrderResponse?> GetOrderByCondition(FilterDefinition<Order> filter);
+    Task<OrderResponse?> AddOrder(OrderAddRequest orderAddRequest);
+    Task<OrderResponse?> UpdateOrder(OrderUpdateRequest orderUpdateRequest);
+    Task<bool> DeleteOrder(Guid orderID);
+
+}
