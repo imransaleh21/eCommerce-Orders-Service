70af43a493cf1e921554a92db484e0d2f8a2dd98
Imran Saleh <imranruet88@gmail.com>
2026-07-06 23:04:38 +0600

    To communicate with user microservice create a DTO and also make a httpClient for communication

---
+++ OrdersMicroService.BusinessLogicLayer/DTOs/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace OrdersMicroService.BusinessLogicLayer.DTOs;
+
+public record UserDTO(
+Guid UserId,
+string? Email,
+string? PersonName,
+string? Gender
+);
---
+++ OrdersMicroService.BusinessLogicLayer/HttpClients/UsersMicroserviceclient.cs
@@ -0,0 +1,36 @@
+﻿using OrdersMicroService.BusinessLogicLayer.DTOs;
+using System.Net.Http.Json;
+
+namespace eCommerce.Core.HttpClients
+{
+    public class UsersMicroserviceclient
+    {
+        private readonly HttpClient _httpClient;
+        public UsersMicroserviceclient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<UserDTO?> GetUserByIdAsync(Guid userId)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"/api/users/{userId}");
+            if (!response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    return null;
+
+                else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                    throw new HttpRequestException($"Bad request: {response.ReasonPhrase}");
+                else
+                    throw new HttpRequestException($"Error retrieving user: {response.ReasonPhrase}");
+            }
+
+            UserDTO? user = await response.Content.ReadFromJsonAsync<UserDTO>();
+            if (user == null)
+            {
+                throw new HttpRequestException("User not found or response content is empty.");
+            }
+            return user;
+        }
+    }
+}
